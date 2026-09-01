a953eb62d2ec91f83b1e447f166205489a567a1f
Krishna Wable <krishna.official0717@gmail.com>
2026-09-01 11:08:46 +0530

    Update next.config.ts
    changes next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
+  typescript: { ignoreBuildErrors: true },
   allowedDevOrigins: ["10.175.69.221", "*.local", "localhost"],
 };
 
