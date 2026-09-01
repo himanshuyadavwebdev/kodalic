69aaa123feace7ed1603adb164f9b86d965c4ac9
Krishna Wable <krishna.official0717@gmail.com>
2026-09-01 11:01:36 +0530

    Update next.config.ts
    'enable static export

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  images: { unoptimized: true },
   allowedDevOrigins: ["10.175.69.221", "*.local", "localhost"],
 };
 
